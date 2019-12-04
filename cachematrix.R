60258ec1290f4df9fd58b6a991d8923bf8ac4f49
shoomow <anthony.shumov@gmail.com>
2019-12-04 23:17:26 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,14 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+   sq <- sqrt(length(x))
 
+  if ((sq - floor(sq)) == 0) {
+    x <- matrix(x,nrow = sq,ncol = sq)
+    x
+  } else {
+    message("Sorry, but your matrix cannot be inverted")
+  }
 }
 
 
